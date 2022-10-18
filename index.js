aac4913fe2973fd55913991888f7fffe70469708
ShahriyarShehab <kshahriyar28@gmail.com>
2022-10-18 13:00:38 +0600

    add npm

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express= require('express')
-const cors= require('cors')
-const bodyParser= require('body-parser')
-const mongoose= require('mongoose')
+const express= require('express');
+const cors= require('cors');
+const bodyParser= require('body-parser');
+const mongoose= require('mongoose');
 
 
-const app = express()
-
-app.get('/',(req,rezs) => {
-  req.send("<h1>App Is Running</h1>")
-})
-
+const app = express();
+/* fro knowing Server Is working or not
+app.get('/',(req,res) => {
+  res.send("<h1>App Is Running</h1>");
+});
+*/
+app.use(cors());
+app.use(express.static('public'));
 const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, ()=> {
-  console.log("APP is running on PORT" + PORT)
-})
+  console.log("APP is running on PORT   http://localhost:" + PORT);
+});
 
